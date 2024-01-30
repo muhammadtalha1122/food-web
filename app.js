fe6a5b28901db47f4fe5f61415400b45542b405b
muhammad talha <mt9898463@gmail.com>
2024-01-30 11:39:44 -0800

    add file

---
+++ app.js
@@ -0,0 +1,10 @@
+let signUp= document.getElementById("SignUp")
+let signIn= document.getElementById("signin")
+
+
+signUp.addEventListener("click",()=>{
+        window.location.href="./auth/signUp/index.html"
+})
+signIn.addEventListener("click",()=>{
+        window.location.href="./auth/signin/index.html"
+})
